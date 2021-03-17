c59517d8d8c71c793c965ef322743b22cd885d85
MicroFish91 <mwfisher91@gmail.com>
2021-03-17 11:10:37 -0700

    Python 102 easy-mid questions

--- Python/python102.py
+++ Python/python102.py
@@ -81,3 +81,81 @@
 
 
 
+# Leetspeak
+# userInputTwo = input("Please enter text to be converted into Leetspeak: ")
+# leetSpeak = ""
+
+# for character in userInputTwo:
+
+#     if character == "A":
+#         character = "4"
+#     elif character == "E":
+#         character = "3"
+#     elif character == "G":
+#         character = "6"
+#     elif (character == "I") or (character == "L"):
+#         character = "1"
+#     elif character == "O":
+#         character = "0"
+#     elif character == "S":
+#         character = "5"
+#     elif character == "T":
+#         character = "7"
+
+#     leetSpeak += character
+
+# print(leetSpeak)
+
+
+
+#Long-long vowels
+userInput = input("What phrase would you like to have long vowels? ")
+
+vowels = ("A", "a", "E", "e", "I", "i", "O", "o", "U", "u")
+vowelNumber = 0
+lastVowel = ""
+output = ""
+    
+for character in userInput:
+
+    vowelCheck = False
+
+    for vowel in vowels:
+
+        # Check if character is a vowel
+        if character == vowel:
+            vowelCheck = True
+
+    # Next Sort the Logic....
+    # If we're not seeing a repea`t vowel
+    if character != lastVowel:
+
+        # If last vowel wasn't repeating, we still need to log it
+        if vowelNumber == 1:
+            output += lastVowel
+            lastVowel = ""
+            vowelNumber = 0
+
+        # If just ending repeat vowel, need to log into output
+        if vowelNumber >= 2:
+            output += lastVowel * 5
+            lastVowel = ""
+            vowelNumber = 0
+
+        # Check to see if new character is a vowel
+        if vowelCheck:
+            vowelNumber += 1
+            lastVowel = character
+        # If not, it must be a consonant
+        else:
+            lastVowel = ""
+            vowelNumber = 0
+            output += character
+
+    # Else, it must be a repeat of the last vowel
+    else: 
+        vowelNumber += 1
+        lastVowel = character
+               
+    
+print(output)
