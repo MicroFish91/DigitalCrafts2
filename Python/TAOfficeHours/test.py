4ed71a0fccbed364ce402de883ee970c20ae0398
MicroFish91 <mwfisher91@gmail.com>
2021-04-15 07:19:26 -0700

    Add rpg game starter code

---
+++ Python/TAOfficeHours/test.py
@@ -0,0 +1,19 @@
+# Typecasting and variable types
+# age = int(input("How old are you? "))
+
+# isinstance()
+
+# Variable type: int, str, object
+
+# print(isinstance(5, int))
+# print(isinstance("5", int))
+# print(isinstance({}, object))
+
+# str()
+# age = 6
+# randomString = "hi"
+# booleanType = True
+
+# "6" + "hi" => "6hi"
+
+# print(str(age) + randomString)
